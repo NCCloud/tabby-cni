762359e680ce27343eff84e739cdc4309d21d24d
Oleg Neichev <59373462+BonySmoke@users.noreply.github.com>
2023-12-21 18:29:20 +0200

    [NOCARD] Prevent Bridge Removal with Attached Interfaces (#13)
    * list bridge ports and remove elements
    
    * fix string formatting
    
    ---------
    
    Co-authored-by: Oleg Neychev <oleg.neychev@namecheap.com>

--- main.go
+++ main.go
@@ -84,7 +84,7 @@ func main() {
 		Client: mgr.GetClient(),
 		Scheme: mgr.GetScheme(),
 	}).SetupWithManager(mgr); err != nil {
-		setupLog.Error(err, "unable to create controller", "controller", "Network")
+		setupLog.Error(err, "unable to create controller", "controller", "NetworkAttachment")
 		os.Exit(1)
 	}
 	//+kubebuilder:scaffold:builder
